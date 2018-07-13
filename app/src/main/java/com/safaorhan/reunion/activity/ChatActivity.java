6e6a13801f7bab14b226cf6460f1ebe868dfb906
Özkan Bölükbaş <ozkanbolukbas@gmail.com>
2018-07-13 18:03:00 +0300

    Activity names added to app bar. Names stored as a string in strings.xml

--- app/src/main/java/com/safaorhan/reunion/activity/ChatActivity.java
+++ app/src/main/java/com/safaorhan/reunion/activity/ChatActivity.java
@@ -34,7 +34,7 @@ public class ChatActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.chat_fab);
         final String documentPath = getIntent().getStringExtra("documentPath");
         String name= getIntent().getStringExtra("name");
-        setTitle(getString(R.string.chat_with)+name);
+        setTitle(getString(R.string.chat_with)+" "+name);
         chatAdapter = ChatAdapter.get(FirebaseFirestore.getInstance().document(documentPath));
         chatAdapter.setListener(new ChatAdapter.OnChatMessageAddedListener() {
             @Override
